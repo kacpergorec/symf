52d2b6f4c801c841a602f9118249c145a3aa3a97
kacper <kacpergorec@gmail.com>
2022-10-13 01:10:15 +0200

    Mailer DSN workaround, verify email validation

---
+++ src/Form/Type/UserEditType.php
@@ -0,0 +1,32 @@
+<?php
+declare (strict_types=1);
+
+namespace App\Form\Type;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, ['attr' => ['placeholder' => 'elon@spacex.com'], 'required' => true])
+            ->add('username', TextType::class, ['attr' => ['placeholder' => 'TheSpaceGuy1971'], 'required' => true])
+            ->add('password', PasswordType::class, ['help' => 'We dont care how secure your password is.', 'required' => true])
+            ->add('save', SubmitType::class, ['label' => 'Sign up']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
