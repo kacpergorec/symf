672c7ca2f328fce47861af37c5689680879f72c0
kacper <kacpergorec@gmail.com>
2022-10-25 02:17:38 +0200

    Expiration dates, token lengh incrementing

---
+++ src/Twig/DateDifferenceExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class DateDifferenceExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('diff_in_days', [$this, 'diffInDays']),
+            new TwigFilter('diff_level', [$this, 'difflevel']),
+            new TwigFilter('diff_error_code', [$this, 'diffError']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('diff_in_days', [$this, 'diffInDays']),
+            new TwigFunction('diff_level', [$this, 'difflevel']),
+            new TwigFunction('diff_error_code', [$this, 'diffError']),
+        ];
+    }
+
+    public function diffInDays(\DateTimeInterface $since = null): int
+    {
+        $today = new \DateTime();
+
+        return $since->diff($today)->format("%a");
+    }
+
+    public function difflevel(\DateTimeInterface $since): string
+    {
+        $days = $this->diffInDays($since);
+
+        if ($days <= 5) {
+            return 'danger';
+        }
+
+        if ($days <= 30) {
+            return 'warning';
+        }
+
+        return '';
+    }
+
+}
